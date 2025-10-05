c98fe4fec490a4a0e19244d4de4d5aa60d2c3b8c
Dennis Graiani <dennis.graiani@gmail.com>
2025-10-05 06:02:52 -0400

    change format of source allocation string

--- packet-livewire.c
+++ packet-livewire.c
@@ -260,11 +260,11 @@ static int dissect_lwadv_msg(tvbuff_t* tvb, packet_info *pinfo, proto_tree *tree
                 proto_item *ti = proto_tree_add_item(tree, hf_lw_busy, tvb, offset + 1, 8, ENC_BIG_ENDIAN);
                 if (tvb_get_uint64(tvb, offset + 1, ENC_BIG_ENDIAN) == 0){
                     // free
-                    proto_item_append_text(ti, " (Free)");
+                    proto_item_append_text(ti, ": Free");
                 }
                 else {
                     ws_in4_addr console_ip;
-                    uint32_t fader_num; 
+                    unsigned fader_num; 
                     char addr_str[15];
                     proto_tree *busy_tree = proto_item_add_subtree(ti, ett_lwadv);
                     proto_tree_add_item_ret_uint(busy_tree, hf_lw_busy_hwid, tvb, offset + 3, 2, ENC_BIG_ENDIAN, &console_ip);
@@ -275,7 +275,7 @@ static int dissect_lwadv_msg(tvbuff_t* tvb, packet_info *pinfo, proto_tree *tree
                     proto_tree_add_ipv4(busy_tree, hf_lw_busy_ip, tvb, offset + 7, 2, console_ip);
                     proto_item *fader = proto_tree_add_item_ret_uint(busy_tree, hf_lw_busy_fader, tvb, offset + 6, 1, ENC_BIG_ENDIAN, &fader_num);
                     proto_item_set_text(fader, "Fader: %d", fader_num + 1);
-                    proto_item_append_text(ti, " (Console %s, Fader %d)", addr_str, fader_num + 1);
+                    proto_item_append_text(ti, ": Console %s, Fader %d", addr_str, fader_num + 1);
                 }
                 return offset + 9;
             }
